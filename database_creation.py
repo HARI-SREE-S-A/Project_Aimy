8b9cb7b40e1a03288ac591c50beb5b10a410b301
HARISREE SURESH <getharisree@outlook.com>
2023-01-31 18:33:06 +0530

    Update database_creation.py

--- database_creation.py
+++ database_creation.py
@@ -1,1 +1,15 @@
+import sqlite3
 
+database = sqlite3.connect("database.db")
+cur = databse.cursor()
+###### variable declaring and assignment ##########
+
+s_no = input("serial number")
+name = input("name")
+batch = input("batch")
+des = input("description")
+
+####### table creation ############
+
+table = ''' CREATE TABLE IF NOT EXISTS notes(s_no INTEGER PRIMARY KEY,name TEXT,batch TEXT,description TEXT);'''
+cur.execute(table)
