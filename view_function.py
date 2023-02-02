8681099185942a3303b8a1d1e15b08a5b96c4642
HARISREE SURESH <getharisree@outlook.com>
2023-02-02 16:14:00 +0530

    Update view_function.py

--- view_function.py
+++ view_function.py
@@ -1,1 +1,13 @@
-
+def view():
+    frame = Tk()
+    frame.geometry("700x700")
+    frame.title("data")
+    r_set  = database.execute("SELECT * from notes;")
+    i = 0
+    for notes in r_set:
+        for j in range(len(notes)):
+            e = Entry(frame, width=10, fg='blue') 
+            e.grid(row=i, column=j) 
+            e.insert(END, notes[j])
+           
+        i=i+1
