455c854473f908235379913b8a27fe17765e1a48
HARISREE SURESH <getharisree@outlook.com>
2023-02-02 16:11:57 +0530

    Update update_database_function.py

--- update_database_function.py
+++ update_database_function.py
@@ -1,13 +1,13 @@
 def dat_up():
+    #getting user input from GUI
     idn = first.get()
-    print(idn)
     name = nme_p.get()
     des = des_p.get()
     
-    table = '''CREATE TABLE IF NOT EXISTS notes(id INTEGER PRIMARY KEY,name TEXT,description TEXT);'''
+    table = '''CREATE TABLE IF NOT EXISTS notes(id INTEGER PRIMARY KEY,name TEXT,description TEXT);'''### creating the table
     cur.execute(table)
     table_data = '''INSERT INTO notes(id,name,description)
-    VALUES(?,?,?)'''
+    VALUES(?,?,?)'''# adding or updating table values
     cur.execute(table_data,(idn,name,des))
     database.commit()
     cur.execute("SELECT * from notes;")
